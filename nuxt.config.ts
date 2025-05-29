eefd7edb121810969da9ade73be48224cb6f0d68
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-05-29 16:57:44 +0200

    fixed indentation on nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,18 @@
 export default defineNuxtModule({
-    css: ['~/assets/css/main.css'],
-    components: {
-        global: true,
-        dirs: ['~/components'],
-        prefix: 'Em'
-    },
-    tailwindcss: {
-        configPath: './tailwind.config.ts'
-    },
-    ui: {
-        prefix: 'Em',
-        colorMode: true,
-    },
-    modules: [
-      '@nuxt/ui',
-    ],
+  css: ['~/assets/css/main.css'],
+  components: {
+    global: true,
+    dirs: ['~/components'],
+    prefix: 'Em'
+  },
+  tailwindcss: {
+    configPath: './tailwind.config.ts'
+  },
+  ui: {
+    prefix: 'Em',
+    colorMode: true,
+  },
+  modules: [
+    '@nuxt/ui',
+  ],
 })
